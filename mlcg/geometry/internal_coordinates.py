be93c33b951071ea7ccfea6905c4d283f92e2412
felixmusil <felix.musil@epfl.ch>
2021-10-07 14:11:28 +0200

    typo in shape

--- mlcg/geometry/internal_coordinates.py
+++ mlcg/geometry/internal_coordinates.py
@@ -5,7 +5,7 @@ from .math_utils import safe_norm, safe_normalization
 @torch.jit.script
 def compute_distance_vectors(pos:torch.Tensor, mapping:torch.Tensor):
     assert mapping.dim() == 2
-    assert mapping.shape[1] == 2
+    assert mapping.shape[0] == 2
 
     dr = pos[mapping[1]] - pos[mapping[0]]
 
@@ -17,18 +17,18 @@ def compute_distance_vectors(pos:torch.Tensor, mapping:torch.Tensor):
 @torch.jit.script
 def compute_bound_distances(pos:torch.Tensor, mapping:torch.Tensor):
     assert mapping.dim() == 2
-    assert mapping.shape[1] == 2
+    assert mapping.shape[0] == 2
 
     dr = pos[mapping[1]] - pos[mapping[0]]
-    distances = safe_norm(dr, dim=1)
+    distances = dr.norm(dim=1)
     return distances
 
 @torch.jit.script
 def compute_bound_angles(pos:torch.Tensor, mapping:torch.Tensor):
     assert mapping.dim() == 2
-    assert mapping.shape[1] == 3
+    assert mapping.shape[0] == 3
 
-    dr1 = pos[mapping[1]] - pos[mapping[0]]
-    dr2 = pos[mapping[2]] - pos[mapping[0]]
+    dr1 = pos[mapping[0]] - pos[mapping[1]]
+    dr2 = pos[mapping[2]] - pos[mapping[1]]
     cos_theta = dr1 @ dr2.t()
     return cos_theta
