dc0a990a320ef9b74c318c05cf10c1eaaff9cba0
nec4 <42926839+nec4@users.noreply.github.com>
2021-10-19 18:47:16 +0200

    __init__ remove line
    Co-authored-by: felixmusil <felix.musil@epfl.ch>

--- mlcg/nn/__init__.py
+++ mlcg/nn/__init__.py
@@ -1,4 +1,3 @@
-# from .prior import Harmonic, Repulsion, HarmonicBonds, HarmonicAngles, _Prior
 from .gradients import GradientsOut
 from .schnet import SchNet, InteractionBlock, CFConv, create_schnet
 from .basis import GaussianBasis, ExpNormalBasis, CosineCutoff
