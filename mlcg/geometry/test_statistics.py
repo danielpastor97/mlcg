test cleanup, leaf variable fix in gradients

mlcg/geometry/test_statistics.py
@@ -1,22 +1,23 @@
-import networkx as nx
-from mlcg.geometry.topology import *
-from mlcg.geometry.statistics import *
+import torch
+import pytest
+import numpy as np
+
+from mlcg.geometry.topology import Topology
+from mlcg.geometry.statistics import compute_statistics, fit_baseline_models
 from mlcg.geometry.statistics import _symmetrise_map
-from mlcg.nn.prior import *
-from mlcg.data import *
+from mlcg.nn.prior import HarmonicBonds, HarmonicAngles, Repulsion
+from mlcg.data.atomic_data import AtomicData
 from mlcg.geometry.utils import ase_bonds2tensor, ase_angles2tensor
+from mlcg.neighbor_list.neighbor_list import make_neighbor_list
 
 from torch_geometric.data.collate import collate
 from ase.build import molecule
 from ase.geometry.analysis import Analysis
 from ase.neighborlist import natural_cutoffs
-import torch
-import pytest
-import numpy as np
 
 # Physical units
 temperature = 350  # K
-#:Boltzmann constan in kcal/mol/K
+#:Boltzmann constant in kcal/mol/K
 kB = 0.0019872041
 beta = 1 / (temperature * kB)
 
@@ -61,14 +62,7 @@ data_list = []
 for frame in range(mock_data_frames.shape[0]):
     neighbor_lists = {}
     for (tag, order, edge_list) in zip(nls_tags, nls_orders, nls_edges):
-        neighbor_lists[tag] = {
-            "tag": tag,
-            "order": order,
-            "index_mapping": edge_list,
-            "cell_shifts": None,
-            "rcut": None,
-            "self_interaction": False,
-        }
+        neighbor_lists[tag] = make_neighbor_list(tag, order, edge_list)
         data_point = AtomicData(
             pos=mock_data_frames[frame],
             atom_types=torch.tensor(test_topo.types),

mlcg/nn/gradients.py
@@ -155,5 +155,5 @@ class GradientsOut(torch.nn.Module):
 
             data.out[self.name][FORCE_KEY] = -dy_dr
             assert not torch.any(torch.isnan(dy_dr)), f"nan in {self.name}"
-        data.pos.requires_grad_(False)
+        data.pos = data.pos.detach()
         return data

mlcg/nn/test_cutoff.py
@@ -1,9 +1,8 @@
-import networkx as nx
 import torch
 import pytest
 import numpy as np
 
-from mlcg.nn.cutoff import *
+from mlcg.nn.cutoff import IdentityCutoff, CosineCutoff
 
 data = torch.randn(100, 3)
 linear_data = torch.linspace(0, 10, 100)

mlcg/nn/test_outs.py
@@ -1,19 +1,19 @@
-import networkx as nx
 import torch
 import pytest
 import numpy as np
-from numpy.random import default_rng
 from torch_geometric.data.collate import collate
 
 from ase.build import molecule
 from mlcg.geometry import Topology
-from mlcg.geometry.statistics import *
+from mlcg.geometry.statistics import fit_baseline_models
 from mlcg.neighbor_list.neighbor_list import make_neighbor_list
-from mlcg.nn import *
-from mlcg.data._keys import *
+from mlcg.nn.prior import HarmonicBonds, HarmonicAngles
+from mlcg.nn.gradients import SumOut, GradientsOut
+from mlcg.data._keys import ENERGY_KEY, FORCE_KEY
+from mlcg.data.atomic_data import AtomicData
 
 # Seeding
-rng = default_rng(94834)
+rng = np.random.default_rng(94834)
 
 # Physical units
 temperature = 350  # K

mlcg/simulation/test_simulation.py
@@ -4,19 +4,24 @@ import tempfile
 import torch
 import pytest
 import numpy as np
-from numpy.random import default_rng
 from torch_geometric.data.collate import collate
 
 from ase.build import molecule
 from mlcg.geometry import Topology
-from mlcg.geometry.statistics import *
+from mlcg.geometry.statistics import fit_baseline_models
 from mlcg.neighbor_list.neighbor_list import make_neighbor_list
-from mlcg.simulation import *
-from mlcg.nn import *
-from mlcg.data._keys import *
+from mlcg.simulation.simulation import (
+    _Simulation,
+    LangevinSimulation,
+    OverdampedSimulation,
+)
+from mlcg.nn.prior import HarmonicBonds, HarmonicAngles
+from mlcg.nn.gradients import SumOut, GradientsOut
+from mlcg.data.atomic_data import AtomicData
+from mlcg.data._keys import FORCE_KEY, MASS_KEY, POSITIONS_KEY, ATOM_TYPE_KEY
 
 # Seeding
-rng = default_rng(94834)
+rng = np.random.default_rng(94834)
 
 # Physical units
 temperature = 350  # K
@@ -51,14 +56,7 @@ prior_data_list = []
 for frame in range(prior_data_frames.shape[0]):
     neighbor_lists = {}
     for (tag, order, edge_list) in zip(nls_tags, nls_orders, nls_edges):
-        neighbor_lists[tag] = {
-            "tag": tag,
-            "order": order,
-            "index_mapping": edge_list,
-            "cell_shifts": None,
-            "rcut": None,
-            "self_interaction": False,
-        }
+        neighbor_lists[tag] = make_neighbor_list(tag, order, edge_list)
     data_point = AtomicData(
         pos=prior_data_frames[frame],
         atom_types=torch.tensor(test_topo.types),