779c0e8b4fc264ed8badcc419e3dc27649ed2dce
nec4 <nec4@rice.edu>
2021-11-16 18:47:09 +0100

    black

--- mlcg/nn/radial_basis.py
+++ mlcg/nn/radial_basis.py
@@ -18,7 +18,9 @@ def visualize_basis(rbf_layer: nn.Module):
     import matplotlib.pyplot as plt
 
     distances = torch.linspace(
-        rbf_layer.cutoff.cutoff_lower - 1, rbf_layer.cutoff.cutoff_upper + 1, 1000
+        rbf_layer.cutoff.cutoff_lower - 1,
+        rbf_layer.cutoff.cutoff_upper + 1,
+        1000,
     )
     expanded_distances = rbf_layer(distances)
 
