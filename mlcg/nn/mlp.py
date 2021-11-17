0fd688e96565a8837692f1448987c6587c4b2d76
nec4 <nec4@rice.edu>
2021-11-17 20:02:52 +0100

    output network parameter reset

--- mlcg/nn/mlp.py
+++ mlcg/nn/mlp.py
@@ -1,9 +1,8 @@
 import torch
-from torch import nn
 from typing import List
 
 
-class MLP(nn.Module):
+class MLP(torch.nn.Module):
     """
     Multilayer Perceptron for regression of scalars
     """
@@ -11,23 +10,30 @@ class MLP(nn.Module):
     def __init__(
         self,
         layer_widths: List[int] = [10, 10, 1],
-        activation_func: nn.Module = torch.nn.Tanh(),
+        activation_func: torch.nn.Module = torch.torch.nn.Tanh(),
     ):
         super(MLP, self).__init__()
         layers = []
         for w_in, w_out in zip(layer_widths[:-2], layer_widths[1:-1]):
-            layers.append(nn.Linear(w_in, w_out, bias=True))
+            layers.append(torch.nn.Linear(w_in, w_out, bias=True))
             layers.append(activation_func)
         # last layer without activation function and bias
         layers.append(
-            nn.Linear(
+            torch.nn.Linear(
                 layer_widths[-2],
                 layer_widths[-1],
                 bias=False,
             )
         )
 
-        self.layers = nn.Sequential(*layers)
+        self.layers = torch.nn.Sequential(*layers)
+        self.reset_parameters()
+
+    def reset_parameters(self):
+        torch.nn.init.xavier_uniform_(self.output_network[0].weight)
+        self.output_network[0].bias.data.fill_(0)
+        torch.nn.init.xavier_uniform_(self.output_network[2].weight)
+        self.output_network[2].bias.data.fill_(0)
 
     def forward(self, x):
         """Forward pass"""
