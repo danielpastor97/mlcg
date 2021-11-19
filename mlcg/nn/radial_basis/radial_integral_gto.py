950af8fcb18137954baac1a7af85b3c6efa91298
felixmusil <felix.musil@epfl.ch>
2021-11-19 22:38:50 +0100

    rework ri gto

--- mlcg/nn/radial_basis/radial_integral_gto.py
+++ mlcg/nn/radial_basis/radial_integral_gto.py
@@ -119,68 +119,53 @@ def splined_radial_integrals(nmax, lmax, rc, sigma, cutoff, mesh_size=600):
     Rnl = NaturalCubicSpline(coeffs)
     return Rnl
 
+def sn(n,rcut,nmax):
+    return rcut*max(np.sqrt(n),1)/nmax
 
-def dn(n, rcut, nmax):
-    sn = rcut * max(np.sqrt(n), 1) / nmax
-    return 0.5 / (sn) ** 2
+def dn(n,rcut,nmax):
+    s_n = sn(n,rcut,nmax)
+    return 0.5/(s_n)**2
 
+def gto_norm(n,rcut,nmax):
+    s_n = sn(n,rcut,nmax)
+    norm2 = 0.5 / (np.power(s_n, 2*n+3)*float(gamma(n+1.5)))
+    return np.sqrt(norm2)
 
-def gto_norm(n, rcut, nmax):
-    d = dn(n, rcut, nmax)
-    return 1 / np.sqrt(
-        np.power(d, -n - 1.5) * np.power(2, -n - 2.5) * float(gamma(n + 1.5))
-    )
-
-
-def ortho_Snn(rcut, nmax):
-    Snn = np.zeros((nmax, nmax))
-    norms = np.array([gto_norm(n, rcut, nmax) for n in range(nmax)])
-    ds = np.array([dn(n, rcut, nmax) for n in range(nmax)])
-    for n, m in product(range(nmax), range(nmax)):
-        Snn[n, m] = (
-            norms[n]
-            * norms[m]
-            * 0.5
-            * np.power(ds[n] + ds[m], -0.5 * (3 + n + m))
-            * float(gamma(0.5 * (3 + m + n)))
-        )
-    eigenvalues, unitary = np.linalg.eig(Snn)
+def ortho_Snn(rcut,nmax):
+    Snn = np.zeros((nmax,nmax))
+    norms = np.array([gto_norm(n,rcut,nmax) for n in range(nmax)])
+    bn = np.array([dn(n,rcut,nmax) for n in range(nmax)])
+    for n,m in product(range(nmax),range(nmax)):
+        Snn[n,m] = norms[n]*norms[m]*0.5*np.power(bn[n]+bn[m], -0.5*(3+n+m)) * float(gamma(0.5*(3+m+n)))
+    eigenvalues, unitary = np.linalg.eigh(Snn)
     diagoverlap = np.diag(np.sqrt(eigenvalues))
-    newoverlap = np.dot(np.conj(unitary), np.dot(diagoverlap, unitary.T))
+    newoverlap = unitary @ diagoverlap @ unitary.T
     orthomatrix = np.linalg.inv(newoverlap)
-    return orthomatrix
+    return orthomatrix, Snn
 
-
-def gto(rcut, nmax, r):
-    ds = np.array([dn(n, rcut, nmax) for n in range(nmax)])
-    ortho = ortho_Snn(rcut, nmax)
-    norms = np.array([gto_norm(n, rcut, nmax) for n in range(nmax)])
-    res = np.zeros((r.shape[0], nmax))
+def gto(rcut,nmax, r):
+    ds = np.array([dn(n,rcut,nmax) for n in range(nmax)])
+    ortho,Snn = ortho_Snn(rcut,nmax)
+    norms = np.array([gto_norm(n,rcut,nmax) for n in range(nmax)])
+    res = np.zeros((r.shape[0],nmax))
     for n in range(nmax):
-        res[:, n] = norms[n] * np.power(r, n) * np.exp(-ds[n] * r ** 2)
-    res = np.dot(res, ortho)
+        res[:,n] = norms[n]*np.power(r,n+1)*np.exp(-ds[n]*r**2)
+    res = res @ ortho
     return res
 
+def ri_gto(n,l,rij,c,d, norm):
+    res = exp(-c*rij**2)*(gamma(0.5*(l+n+3))/gamma(l+1.5)) * power(c*rij,l) * power(c+d,-0.5*(l+n+3))
+    res *= hyp1f1(0.5*(n+l+3),l+1.5,power(c*rij,2)/(c+d))
+    return norm*float(res)
 
-def ri_gto(n, l, rij, c, d, norm):
-    res = (
-        exp(-c * rij ** 2)
-        * (gamma(0.5 * (l + n + 3)) / gamma(l + 1.5))
-        * power(c * rij, l)
-        * power(c + d, -0.5 * (l + n + 3))
-    )
-    res *= hyp1f1(0.5 * (n + l + 3), l + 1.5, power(c * rij, 2) / (c + d))
-    return norm * float(res)
-
-
-def o_ri_gto(rcut, nmax, lmax, rij, c):
-    ds = np.array([dn(n, rcut, nmax) for n in range(nmax)])
-    norms = np.array([gto_norm(n, rcut, nmax) for n in range(nmax)])
-    ortho = ortho_Snn(rcut, nmax)
-    res = np.zeros((rij.shape[0], lmax, nmax))
-    for ii, dist in enumerate(rij):
+def o_ri_gto(rcut,nmax,lmax,rij,c):
+    ds = np.array([dn(n,rcut,nmax) for n in range(nmax)])
+    norms = np.array([gto_norm(n,rcut,nmax) for n in range(nmax)])
+    ortho,Snn = ortho_Snn(rcut,nmax)
+    res = np.zeros((rij.shape[0],lmax,nmax))
+    for ii,dist in enumerate(rij):
         for l in range(lmax):
             for n in range(nmax):
-                res[ii, l, n] = ri_gto(n, l, float(dist), c, ds[n], norms[n])
-    res = np.dot(res, ortho)
+                res[ii,l,n] = ri_gto(n,l,float(dist),c,ds[n],norms[n])
+    res = res @ ortho
     return res
