32810bfe994170688f73a3380ecaedfc9cb514ac
Atharva Kelkar <atharva.kelkar@gmail.com>
2022-01-25 10:16:42 +0530

    Added ForceMSE loss to __init__ for ease of import

--- mlcg/nn/__init__.py
+++ mlcg/nn/__init__.py
@@ -2,7 +2,7 @@ from .gradients import GradientsOut, SumOut
 from .schnet import SchNet, StandardSchNet
 from .radial_basis import GaussianBasis, ExpNormalBasis
 from .cutoff import CosineCutoff, IdentityCutoff
-from .losses import ForceRMSE, Loss
+from .losses import ForceRMSE, Loss, ForceMSE
 from .prior import Harmonic, HarmonicAngles, HarmonicBonds, Repulsion, Dihedral
 from .mlp import MLP
 
