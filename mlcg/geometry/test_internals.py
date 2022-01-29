b8dbbdb3096f39913050bf86b339fc822d494dfa
clarktemple03 <clarktemple03@gmail.com>
2022-01-29 21:51:56 +0100

    Test for dihedral fitting

--- mlcg/geometry/test_internals.py
+++ mlcg/geometry/test_internals.py
@@ -2,7 +2,10 @@ import torch
 import numpy as np
 import mdtraj
 from mdtraj.core.element import carbon
-from mlcg.geometry.internal_coordinates import *
+from mlcg.geometry.internal_coordinates import compute_torsions
+import copy
+from mlcg.geometry.statistics import _get_bin_centers
+from mlcg.nn.prior import Dihedrals
 
 # Mock data of ten atoms for 1000 frames
 n_frames = 1000
@@ -15,7 +18,7 @@ dihedral_atoms = np.array(
 )
 mapping = torch.tensor(
     np.concatenate(
-        [dihedral_atoms.T + (10 * i) for i in range(n_frames)], axis=1
+        [dihedral_atoms.T + (n_atoms * i) for i in range(n_frames)], axis=1
     )
 )
 
@@ -39,3 +42,81 @@ def test_mdtraj_dihedral_compatibility():
     )
     mdtraj_dihedrals = mdtraj.compute_dihedrals(traj, dihedral_atoms)
     np.testing.assert_allclose(mlcg_dihedrals, mdtraj_dihedrals, rtol=1e-3)
+
+def test_dihedral_aic_criterion():
+    '''
+        Test to ensure that proper index of fitting is choosen
+        Perturb coordinates of first dihedral with a 3-amplitude sine wave
+    '''
+    TargetPrior = Dihedrals
+
+    test_coords = np.random.randn(n_frames, n_atoms, 3).astype("float32")
+    torch_coords = torch.tensor(test_coords.reshape(n_frames * n_atoms, 3))
+    torch_coords[1::n_atoms] = torch.tensor((0,0,0)) 
+    torch_coords[2::n_atoms] = torch.tensor((1,0,0)) 
+    torch_coords[3::n_atoms] = torch.tensor((2,1,0)) 
+    pos = torch_coords[:4]
+    ind_mapping = torch.tensor((0,1,2,3))
+    ind_mapping = torch.reshape(ind_mapping,[4,1])
+    theta_map = theta_mapping(pos,ind_mapping)
+    k1s = [1,2,3]
+    samples = sample_from_curve(k1s,n_frames)
+    for i_s,sample in enumerate(samples):
+        for k,v in theta_map.items():
+            if np.abs(sample,v) < 1e-2:
+                torch_coords[i_s*n_atoms] = torch.stack(k)
+
+    values = TargetPrior.compute_features(torch_coords, mapping)
+    b_min = values.min()
+    b_max = values.max()
+    nbins = 100
+    bin_centers = _get_bin_centers(values, nbins, b_min=b_min, b_max=b_max)
+    hist = torch.histc(values, bins=nbins, min=b_min, max=b_max)
+
+    mask = hist > 0
+    bin_centers_nz = bin_centers[mask]
+
+    ncounts_nz = hist[mask]
+    dG_nz = -torch.log(ncounts_nz)
+    params = TargetPrior.fit_from_potential_estimates(
+        bin_centers_nz, dG_nz
+    )
+    assert all(params['k1s'][4:] == 0)
+
+def sample_from_curve(k1s,n_frames):
+    V = 0
+    thetas = torch.from_numpy(np.linspace(-np.pi,np.pi,100))
+    for ik,k1 in enumerate(k1s):
+        V += k1*torch.sin(ik*thetas)
+    pi = torch.exp(-V)
+    pi = pi/torch.sum(pi)
+    cum_pi = torch.cumsum(pi,dim=0).numpy()
+    theta = []
+    for _ in range(n_frames):
+        sel_ind = np.where(cum_pi>np.random.rand())[0][0]
+        theta.append(thetas[sel_ind].numpy())
+    return theta
+
+def theta_mapping(pos,mapping):
+    theta_map = {}
+    r = 1
+    thetas = torch.linspace(-np.pi,np.pi,100)
+    for thet in thetas:
+        x0 = torch.tensor(-1)
+        y0 = r*torch.cos(thet)
+        z0 = r*torch.sin(thet)
+        pos[0] = torch.tensor((x0,y0,z0))
+        dr1 = pos[mapping[1]] - pos[mapping[0]]
+        dr1 = dr1 / dr1.norm(p=2, dim=1)[:, None]
+        dr2 = pos[mapping[2]] - pos[mapping[1]]
+        dr2 = dr2 / dr2.norm(p=2, dim=1)[:, None]
+        dr3 = pos[mapping[3]] - pos[mapping[2]]
+        dr3 = dr3 / dr3.norm(p=2, dim=1)[:, None]
+        n1 = torch.cross(dr1, dr2, dim=1)
+        n2 = torch.cross(dr2, dr3, dim=1)
+        m1 = torch.cross(n1, dr2, dim=1)
+        y = torch.sum(m1 * n2, dim=-1)
+        x = torch.sum(n1 * n2, dim=-1)
+        theta = torch.atan2(-y, x)
+        theta_map[(x0,y0,z0)] = theta.numpy()
+    return theta_map
