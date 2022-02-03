f3917786688742bc73a30a7199950defcacb3685
nec4 <nec4@rice.edu>
2022-02-03 03:03:59 +0100

    added kfold splitter and docs

--- examples/h5_pl/partition_gen.py
+++ examples/h5_pl/partition_gen.py
@@ -2,12 +2,18 @@ import numpy, h5py
 from mlcg.datasets import multimol_split
 
 
-
-f = h5py.File("/import/a12/users/nickc/mlcg_delta_datasets/dihedral_1_6_res_exclusion/combined_dihedral_1_6_res_exclusion.h5")
+f = h5py.File(
+    "/import/a12/users/nickc/mlcg_delta_datasets/dihedral_1_6_res_exclusion/combined_dihedral_1_6_res_exclusion.h5"
+)
 
 mol_dict = {}
 for k in f["CATH"]:
     mol_dict[k] = f["CATH"][k].attrs["N_frames"]
 
-split_dict = multimol_split(mol_dict, proportions={"train":0.8, "val":0.2}, random_seed=5513, verbose=True)
+split_dict = multimol_split(
+    mol_dict,
+    proportions={"train": 0.8, "val": 0.2},
+    random_seed=5513,
+    verbose=True,
+)
 print(split_dict)
--- mlcg/datasets/__init__.py
+++ mlcg/datasets/__init__.py
@@ -2,4 +2,10 @@ from .chignolin import ChignolinDataset
 from .h5_dataset import H5PartitionDataLoader, H5Dataset
 from .split_utils import mol_split, multimol_split
 
-__all__ = ["ChignolinDataset", "H5PartitionDataLoader", "H5Dataset", "mol_split", "multimol_split"]
+__all__ = [
+    "ChignolinDataset",
+    "H5PartitionDataLoader",
+    "H5Dataset",
+    "mol_split",
+    "multimol_split",
+]
