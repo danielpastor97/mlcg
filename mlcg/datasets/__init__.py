0c3ddbb173d199171364408e7849da259c5ebf82
nec4 <nec4@rice.edu>
2022-02-08 18:03:41 +0100

    black

--- mlcg/datasets/__init__.py
+++ mlcg/datasets/__init__.py
@@ -2,4 +2,9 @@ from .chignolin import ChignolinDataset
 from .alanine_dipeptide import AlanineDataset
 from .h5_dataset import H5PartitionDataLoader, H5Dataset
 
-__all__ = ["ChignolinDataset", "H5PartitionDataLoader", "H5Dataset", "AlanineDataset"]
+__all__ = [
+    "ChignolinDataset",
+    "H5PartitionDataLoader",
+    "H5Dataset",
+    "AlanineDataset",
+]
