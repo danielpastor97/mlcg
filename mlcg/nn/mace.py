536f60700d0dee7811678c939fbdc1a5796cc2a1
felixmusil <felix.musil@epfl.ch>
2022-07-08 19:06:28 +0200

    Update mlcg/nn/mace.py
    Co-authored-by: nec4 <42926839+nec4@users.noreply.github.com>

--- mlcg/nn/mace.py
+++ mlcg/nn/mace.py
@@ -26,6 +26,30 @@ from ..neighbor_list.neighbor_list import (
 
 
 class MACEInterface(torch.nn.Module):
+    """MLCG-Tools interface for MACE model. Based on:
+    
+    https://arxiv.org/abs/2206.07697
+    
+    and the corresponding implementation found here:
+    
+    https://github.com/ACEsuit/mace
+    
+    Parameters
+    ------------
+    config:
+        Dictionary of MACE model configuration options
+     gate:
+         Gate function (eg, composition of nonlinearities) to be used in the 
+          EquivariantProductBasisBlock
+     max_num_neighbors:
+         Maximum number of neighbors to return for a
+         given node/atom when constructing the molecular graph during forward
+         passes. This attribute is passed to the torch_cluster radius_graph
+         routine keyword max_num_neighbors, which normally defaults to 32.
+         Users should set this to higher values if they are using higher upper
+         distance cutoffs and expect more than 32 neighbors per node/atom.
+     """
+
     name: Final[str] = "mace"
 
     def __init__(
