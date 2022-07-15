92df77c6dc9ba3d25fd95a6f0db9f26dafad33a7
nec4 <nec4@rice.edu>
2022-07-15 21:00:51 +0200

    __init__ MACE import catch

--- mlcg/nn/__init__.py
+++ mlcg/nn/__init__.py
@@ -5,7 +5,12 @@ from .cutoff import CosineCutoff, IdentityCutoff
 from .losses import ForceMSE, ForceRMSE, Loss
 from .prior import Harmonic, HarmonicAngles, HarmonicBonds, Repulsion, Dihedral
 from .mlp import MLP
-from .mace_interface import MACEInterface
+
+try:
+    from .mace_interface import MACEInterface
+except Exception as e:
+    print(e)
+    print("MACE installation not found ...")
 
 __all__ = [
     "GradientsOut",
