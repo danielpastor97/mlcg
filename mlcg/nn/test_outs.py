8a695c02eb1e40747a558914837eadf659ad866a
Félix <felix.musil@epfl.ch>
2022-07-26 12:12:37 +0200

    fix mace test

--- mlcg/nn/test_outs.py
+++ mlcg/nn/test_outs.py
@@ -30,7 +30,7 @@ class DummyGradientModel(object):
         self.name = model_name
 
 
-HAS_MACE = False
+HAS_MACE = True
 try:
     import mace
     from mlcg.nn.mace_interface import MACEInterface
@@ -39,25 +39,24 @@ try:
         "r_max": 10,
         "num_bessel": 10,
         "num_polynomial_cutoff": 5,
-        "max_ell": 2,
-        "interaction_cls": mace.modules.blocks.RealAgnosticResidualInteractionBlock,
-        "interaction_cls_first": mace.modules.blocks.RealAgnosticInteractionBlock,
+        "max_ell": 1,
+        "interaction_cls": "RealAgnosticResidualInteractionBlock",
+        "interaction_cls_first": "RealAgnosticInteractionBlock",
         "num_interactions": 1,
         "num_elements": len(unique_test_types),
-        "hidden_irreps": "256x0e",
+        "hidden_irreps": "32x0e",
         "MLP_irreps": "16x0e",
         "avg_num_neighbors": 9,
-        "correlation": 4,
-        "gate": torch.nn.Tanh,
+        "correlation": 3,
         "atomic_numbers": unique_test_types,
     }
-    mace_model = MACEInterface(config=mace_config, gate=torch.nn.SiLU)
+    mace_model = MACEInterface(config=mace_config, gate=torch.nn.Tanh())
     mace_force_model = GradientsOut(mace_model, targets=[FORCE_KEY]).float()
-    HAS_MACE = True
-except Exception as e:
+except ImportError as e:
     print(e)
     mace_force_model = DummyGradientModel("mace")
     print("MACE installation not found")
+    HAS_MACE = False
 
 standard_cutoff = CosineCutoff(cutoff_lower=0, cutoff_upper=5)
 standard_basis = GaussianBasis(cutoff=standard_cutoff)
@@ -266,7 +265,7 @@ def test_sum_outs(ASE_prior_model, network_model, out_targets):
 
     # Test to make sure the the aggregate data matches the target totals
     for target in out_targets:
-        np.testing.assert_array_equal(
+        np.testing.assert_allclose(
             target_totals[target].detach().numpy(),
             collated_data_2.out[target].detach().numpy(),
         )
