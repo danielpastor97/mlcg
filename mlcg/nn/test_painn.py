d209fbab22bf22ac0ffbe56b80a3854397690d6b
Jacopo Venturin <venturinjacopo@gmail.com>
2024-08-13 11:25:20 +0200

    included test_painn.py

---
+++ mlcg/nn/test_painn.py
@@ -0,0 +1,140 @@
+import torch
+from torch_geometric.data.collate import collate
+import pytest
+from typing import List
+import warnings
+
+from mlcg.nn.painn import StandardPaiNN
+from mlcg.nn.radial_basis import GaussianBasis
+from mlcg.nn.gradients import GradientsOut
+from mlcg.nn.cutoff import IdentityCutoff, CosineCutoff
+from mlcg.geometry import Topology
+from mlcg.data.atomic_data import AtomicData
+from mlcg.data._keys import ENERGY_KEY, FORCE_KEY
+from ase.build import molecule
+
+
+class MolDatabase(object):
+    """Container for ASE molecules for testing"""
+
+    def __init__(
+        self,
+        mol_names: List[str] = [
+            "AlF3",
+            "C2H3",
+            "ClF",
+            "PF3",
+            "PH2",
+            "CH3CN",
+            "cyclobutene",
+            "CH3ONO",
+            "SiH3",
+            "C3H6_D3h",
+            "CO2",
+            "NO",
+            "trans-butane",
+            "H2CCHCl",
+            "LiH",
+            "NH2",
+            "CH",
+            "CH2OCH2",
+            "C6H6",
+            "CH3CONH2",
+            "cyclobutane",
+            "H2CCHCN",
+            "butadiene",
+            "C",
+            "H2CO",
+            "CH3COOH",
+            "HCF3",
+            "CH3S",
+            "CS2",
+        ],
+    ):
+        self.mol_names = mol_names
+        self.molecules = [molecule(name) for name in self.mol_names]
+        self.mol_topos = [Topology.from_ase(mol) for mol in self.molecules]
+        self.data_list = []
+        data_list = []
+        for mol, topo in zip(self.molecules, self.mol_topos):
+            neighbor_list = topo.neighbor_list("fully connected")
+            data = AtomicData(
+                pos=torch.tensor(mol.get_positions()).float(),
+                atom_types=torch.tensor(mol.get_atomic_numbers()),
+                neighbor_list=neighbor_list,
+            )
+            data_list.append(data)
+
+        self.collated_data, _, _ = collate(
+            data_list[0].__class__,
+            data_list=data_list,
+            increment=True,
+            add_batch=True,
+        )
+        self.force_shape = self.collated_data.pos.shape
+        self.energy_shape = torch.Size([len(self.molecules)])
+        self.atomic_numbers = sorted(
+            torch.unique(self.collated_data.atom_types).numpy().tolist()
+        )
+
+
+standard_basis = GaussianBasis(cutoff=5)
+standard_cutoff = IdentityCutoff(cutoff_lower=0, cutoff_upper=5)
+
+# prepare some data with ASE
+
+database = MolDatabase()
+
+
+@pytest.mark.parametrize(
+    "basis, cutoff, expected_warning",
+    [
+        (GaussianBasis(IdentityCutoff(1, 5)), CosineCutoff(0, 5), UserWarning),
+    ],
+)
+def test_cutoff_warning(basis, cutoff, expected_warning):
+    with pytest.warns(expected_warning):
+        StandardPaiNN(basis, cutoff, [128, 128])
+
+
+@pytest.mark.parametrize(
+    "basis, cutoff, expected_warning",
+    [
+        (GaussianBasis(IdentityCutoff(0, 5)), CosineCutoff(0, 5), None),
+    ],
+)
+def test_cutoff_warning_None(basis, cutoff, expected_warning):
+    with warnings.catch_warnings(record=True):
+        StandardPaiNN(basis, cutoff, [128, 128])
+
+
+def test_minimum_interaction_block():
+    with pytest.raises(ValueError):
+        StandardPaiNN(
+            standard_basis, standard_cutoff, [128, 128], num_interactions=-1
+        )
+
+
+@pytest.mark.parametrize(
+    "collated_data, out_keys, expected_shapes",
+    [
+        (
+            database.collated_data,
+            [ENERGY_KEY, FORCE_KEY],
+            [database.energy_shape, database.force_shape],
+        )
+    ],
+)
+def test_prediction(collated_data, out_keys, expected_shapes):
+    """Test to make sure that the output dictionary is properly populated
+    and that the correspdonding shapes of the outputs are correct given the
+    requested gradient targets.
+    """
+    test_schnet = StandardPaiNN(standard_basis, standard_cutoff, [128, 128])
+    model = GradientsOut(test_schnet, targets=FORCE_KEY).double()
+    collated_data = model(collated_data)
+    assert len(collated_data.out) != 0
+    assert "PaiNN" in collated_data.out.keys()
+    for key, shape in zip(out_keys, expected_shapes):
+        assert key in collated_data.out[model.name].keys()
+        assert collated_data.out[model.name][key].shape == shape
