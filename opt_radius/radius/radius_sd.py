143d00be1759559c461f2ea5398afb1b8f9d7671
Paul Mifsud <p.mifsud@fz-juelich.de>
2024-11-05 22:46:38 +0100

    added optimized radius_graph and distance calculation

---
+++ opt_radius/radius/radius_sd.py
@@ -0,0 +1,41 @@
+from typing import Optional
+import torch
+
+from torch.utils.cpp_extension import load
+mc = load(name='radius_kernel', 
+          sources=['cu/radius_sd.cu'],
+          extra_cflags=['-O3'], 
+          extra_cuda_cflags=['-O3'] 
+          #extra_cuda_cflags=['-arch=compute_89', 
+          #                   '-code=sm_89']
+         ) 
+
+def radius_graph(
+    x: torch.Tensor,
+    r: float,
+    batch: Optional[torch.Tensor] = None,
+    loop: bool = False,
+    max_num_neighbors: int = 32,
+    num_workers: int = 1,
+    batch_size: Optional[int] = None,
+) -> torch.Tensor:
+
+    if x.numel() == 0:
+        return torch.empty(2, 0, dtype=torch.long, device=x.device)
+
+    if batch_size is None:
+        batch_size = 1
+        if batch is not None:
+            assert x.size(0) == batch.numel()
+            batch_size = int(batch.max()) + 1
+    assert batch_size > 0
+    ptr_x: Optional[torch.Tensor] = None
+
+    if batch_size > 1:
+        assert batch is not None
+        arange = torch.arange(batch_size + 1, device=x.device)
+        ptr_x = torch.bucketize(arange, batch)
+
+    return mc.radius_cuda(x, ptr_x, r,
+                          max_num_neighbors,
+                          not loop)
