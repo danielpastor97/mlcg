945b571c6ca1ba717815807d40d28f8db3387203
Yaoyi Chen <29800192+yaoyic@users.noreply.github.com>
2024-11-18 16:01:39 +0100

    more black for external commits...

--- mlcg/simulation/parallel_tempering.py
+++ mlcg/simulation/parallel_tempering.py
@@ -146,13 +146,11 @@ class PTSimulation(LangevinSimulation):
 
         else:
             # Initialize velocities according to Maxwell-Boltzmann distribution
-            self.initial_data[
-                VELOCITY_KEY
-            ] = LangevinSimulation.sample_maxwell_boltzmann(
-                self.beta.repeat_interleave(self.n_atoms),
-                self.initial_data[MASS_KEY],
-            ).to(
-                self.dtype
+            self.initial_data[VELOCITY_KEY] = (
+                LangevinSimulation.sample_maxwell_boltzmann(
+                    self.beta.repeat_interleave(self.n_atoms),
+                    self.initial_data[MASS_KEY],
+                ).to(self.dtype)
             )
 
         self.initial_data[MASS_KEY] = self.initial_data[MASS_KEY].to(self.dtype)
@@ -224,13 +222,11 @@ class PTSimulation(LangevinSimulation):
         self.n_dims = new_configurations[0].pos.shape[1]
 
         # Initialize velocities according to Maxwell-Boltzmann distribution
-        self.initial_data[
-            VELOCITY_KEY
-        ] = LangevinSimulation.sample_maxwell_boltzmann(
-            self.beta.repeat_interleave(self.n_atoms),
-            self.initial_data[MASS_KEY],
-        ).to(
-            self.dtype
+        self.initial_data[VELOCITY_KEY] = (
+            LangevinSimulation.sample_maxwell_boltzmann(
+                self.beta.repeat_interleave(self.n_atoms),
+                self.initial_data[MASS_KEY],
+            ).to(self.dtype)
         )
 
         self.initial_data[MASS_KEY] = self.initial_data[MASS_KEY].to(self.dtype)
