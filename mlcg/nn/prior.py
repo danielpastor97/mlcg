fixed bug in cell shift computation with batch=None and unit cell shift

mlcg/nn/prior.py
@@ -170,7 +170,14 @@ class Harmonic(torch.nn.Module, _Prior):
         return data
 
     @staticmethod
-    def compute_features(pos, mapping, target, pbc=None, cell=None, batch=None):
+    def compute_features(
+        pos: torch.Tensor,
+        mapping: torch.Tensor,
+        target: str,
+        pbc: Optional[torch.Tensor] = None,
+        cell: Optional[torch.Tensor] = None,
+        batch: Optional[torch.Tensor] = None,
+    ) -> torch.Tensor:
         if all([feat != None for feat in [pbc, cell]]):
             cell_shifts = compute_cell_shifts(pos, mapping, pbc, cell, batch)
         else:
@@ -545,7 +552,13 @@ class Repulsion(torch.nn.Module, _Prior):
         return data
 
     @staticmethod
-    def compute_features(pos, mapping, pbc=None, cell=None, batch=None):
+    def compute_features(
+        pos: torch.Tensor,
+        mapping: torch.Tensor,
+        pbc: Optional[torch.Tensor] = None,
+        cell: Optional[torch.Tensor] = None,
+        batch: Optional[torch.Tensor] = None,
+    ) -> torch.Tensor:
         if all([feat != None for feat in [pbc, cell]]):
             cell_shifts = compute_cell_shifts(pos, mapping, pbc, cell, batch)
         else:
@@ -870,9 +883,10 @@ class Dihedral(torch.nn.Module, _Prior):
         mapping: torch.Tensor,
         pbc: Optional[torch.Tensor] = None,
         cell: Optional[torch.Tensor] = None,
+        batch: Optional[torch.Tensor] = None,
     ) -> torch.Tensor:
         if all([feat != None for feat in [pbc, cell]]):
-            cell_shifts = compute_cell_shifts(pos, mapping, pbc, cell)
+            cell_shifts = compute_cell_shifts(pos, mapping, pbc, cell, batch)
         else:
             cell_shifts = None
         return compute_torsions(
@@ -1219,6 +1233,8 @@ def compute_cell_shifts(
         cell_shifts = torch.zeros(
             atom_groups, 3, mapping_order, dtype=pos.dtype
         ).to(pos.device)
+        if batch == None:
+            batch = torch.zeros(pos.shape[0], dtype=int)
         batch_ids = batch[mapping[0]]
         cell_inv = torch.linalg.inv(cell[batch_ids])
         for ii in range(1, cell_shifts.shape[-1]):
@@ -1230,7 +1246,7 @@ def compute_cell_shifts(
                 drs,
                 )
             # compute unit number of unit cell shifts
-            cell_shifts[:, :, ii] = (frac_dr + 0.5) % 0.5
+            cell_shifts[:, :, ii] = torch.floor(frac_dr + 0.5)
             # convert back to cartesian displacement 
             cell_shifts[:, :, ii] = pbc[batch_ids] * torch.einsum(
                 "bij,bj->bi",