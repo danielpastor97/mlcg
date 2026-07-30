a681aa31d7cd65aabf9da61a4e71db063be46c65
sayeg84 <sayeg_84@hotmail.com>
2026-07-30 12:04:15 +0200

    Fix: correct PBCs in prior

--- src/mlcg/nn/prior/repulsion.py
+++ src/mlcg/nn/prior/repulsion.py
@@ -118,6 +118,7 @@ class Repulsion(_Prior):
             mapping=mapping,
             cell_shifts=cell_shifts
         )
+    
     @staticmethod
     def compute(x, sigma):
         """Method defining the repulsion interaction"""
@@ -380,9 +381,17 @@ class ExpRepulsion(_Prior):
         """Computes features for the harmonic interaction from
         an AtomicData instance)
         """
-
         mapping = data.neighbor_list[self.name]["index_mapping"]
-        return ExpRepulsion.compute_features(data.pos, mapping)
+        pbc = getattr(data, "pbc", None)
+        cell = getattr(data, "cell", None)
+        return self.compute_features(
+            pos=data.pos,
+            mapping=mapping,
+            pbc=pbc,
+            cell=cell,
+            batch=data.batch
+        )
+    
 
     def forward(self, data: AtomicData) -> AtomicData:
         """Forward pass through the repulsion interaction.
@@ -419,8 +428,20 @@ class ExpRepulsion(_Prior):
         return data
 
     @staticmethod
-    def compute_features(pos, mapping):
-        return compute_distances(pos, mapping)
+    def compute_features(
+        pos: torch.Tensor,
+        mapping: torch.Tensor,
+        pbc: Optional[torch.Tensor] = None,
+        cell: Optional[torch.Tensor] = None,
+        batch: Optional[torch.Tensor] = None,
+    ) -> torch.Tensor:
+        cell_shifts = _Prior._get_cell_shifts(pos, mapping, pbc, cell, batch)
+        return compute_distances(
+            pos=pos,
+            mapping=mapping,
+            cell_shifts=cell_shifts
+        )
+    
 
     @staticmethod
     def compute(x, alpha, r_0):
